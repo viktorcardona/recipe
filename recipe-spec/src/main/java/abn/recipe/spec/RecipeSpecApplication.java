84cc346d8564fa62ba58a0f144ed93b24a100635
Victor Cardona <victorcardona@victors-air.home>
2025-03-29 23:44:17 +0100

    Recipe API Spec

---
+++ recipe-spec/src/main/java/abn/recipe/spec/RecipeSpecApplication.java
@@ -0,0 +1,13 @@
+package abn.recipe.spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSpecApplication.class, args);
+	}
+
+}
